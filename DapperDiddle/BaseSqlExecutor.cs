3e2e07ecadb502b5e07883b3d254212eb5c18e22
Caybo Kotze <kotzecabo@gmail.com>
2020-12-17 17:24:39 +0200

    :tada: Add the BaseSqlExecutor class

---
+++ DapperDiddle/BaseSqlExecutor.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+using Dapper;
+
+namespace DapperDiddle
+{
+    public class BaseSqlExecutor
+    {
+        private IDbConnection Connection { get; }
+
+        public BaseSqlExecutor(IDbConnection connection)
+        {
+            Connection = connection;
+        }
+
+        public T SelectQuery<T>(string sql, object parameters = null)
+        {
+            return (T)Connection.Query(sql, parameters);
+        }
+
+        public IDbConnection ReturnConnectionInstance()
+        {
+            return Connection;
+        }
+
+        public int Execute(string sql, object parameters = null)
+        {
+            return Connection.Execute(sql, parameters);
+        }
+    }
+}
