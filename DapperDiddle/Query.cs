0c7e3bb18dcb7fec3d9ef09a868776bdc979420f
Caybo Kotze <kotzecabo@gmail.com>
2020-12-17 17:38:04 +0200

    :tada: Add the Query Class

---
+++ DapperDiddle/Query.cs
@@ -0,0 +1,14 @@
+﻿namespace DapperDiddle
+{
+    public abstract class Query<T> : Query
+    {
+        public T Result { get; set; }
+    }
+    
+    public abstract class Query : BaseSqlExecutor
+    {
+        public IQueryExecutor QueryExecutor { get; set; }
+
+        public abstract void Execute();
+    }
+}
