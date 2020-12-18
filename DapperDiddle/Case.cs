d4b29d12e7734431665393f648fc97d89bbc63bf
Caybo Kotze <kotzecabo@gmail.com>
2020-12-18 15:25:25 +0200

    Add some casery logic

---
+++ DapperDiddle/Case.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Globalization;
+using MySql.Data.MySqlClient;
+
+namespace DapperDiddle
+{
+    public enum Case
+    {
+        IgnoreCase,
+        Uppercase,
+        PascalCase,
+        Lowercase,
+        CamelCase
+    }
+    
+    public static class CaseHelpers
+    {
+        public static string ConvertCase(this string value, Case casing)
+        {
+            switch (casing)
+            {
+                case Case.Lowercase:
+                    return value.ToLowerInvariant();
+                case Case.Uppercase:
+                    return value.ToUpperInvariant();
+                case Case.CamelCase:
+                    return char.ToLowerInvariant(value[0]) + value.Substring(1);
+                case Case.PascalCase:
+                {
+                    TextInfo textInfo = new CultureInfo("en-Us", false).TextInfo;
+                    return textInfo.ToTitleCase(value);
+                }
+                case Case.IgnoreCase:
+                    return value;
+            }
+
+            return value;
+        }
+    }
+}
--- DapperDiddle/CommandBuilders.cs
+++ DapperDiddle/CommandBuilders.cs
@@ -1,16 +1,29 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Data;
+using System.Globalization;
 using System.IO;
+using System.Text;
 using System.Windows.Input;
 using DapperDiddle.Commands;
 using Org.BouncyCastle.Asn1.X509.Qualified;
+using PluralizeService.Core;
 
 namespace DapperDiddle
 {
     public static class CommandBuilders
     {
-        public static string BuildInsert<T>(this Command command, string table = null)
+        public static string BuildInsert<T>(this Command command)
+        {
+            return BuildInsert<T>(command, null, Case.Lowercase);
+        }
+        
+        public static string BuildInsert<T>(this Command command, Case casing)
+        {
+            return BuildInsert<T>(command, null, casing);
+        }
+        
+        private static string BuildInsert<T>(this Command command, string table, Case casing)
         {
             var dt = typeof(T).ObjectToDataTable();
 
@@ -18,6 +31,13 @@ namespace DapperDiddle
             {
                 Console.WriteLine(item.DataType);
             }
+
+            if (table is null)
+                table = typeof(T).Name.Pluralize().ConvertCase(Case.Lowercase);
+            
+            var sql = new StringBuilder($"INSERT INTO {table}");
+            
+            
             
             return "";
         }
@@ -37,15 +57,20 @@ namespace DapperDiddle
     {
         public static DataTable ObjectToDataTable(this Type obj)
         {
-            var props = TypeDescriptor.GetProperties(obj);
             var dt = new DataTable();
-
+            var props = TypeDescriptor.GetProperties(obj);
+            
             foreach (PropertyDescriptor p in props)
             {
                 dt.Columns.Add(p.Name, p.PropertyType);
             }
 
             return dt;
+        }
+
+        public static string Pluralize(this string value)
+        {
+            return PluralizationProvider.Pluralize(value);
         }
     }
 }
